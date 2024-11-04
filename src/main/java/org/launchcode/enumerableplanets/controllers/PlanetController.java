0e72d2467026ac13aa06abe6f57dbe3959265b07
Michael Lambert <mlambert125@live.com>
2024-11-04 08:25:42 -0500

    Fixing directory layout

--- src/main/java/org/launchcode/enumerableplanets/controllers/PlanetController.java
+++ src/main/java/org/launchcode/enumerableplanets/controllers/PlanetController.java
@@ -1,4 +1,4 @@
-package org.launchcode.controllers;
+package org.launchcode.enumerableplanets.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
